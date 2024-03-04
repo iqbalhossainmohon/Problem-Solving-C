05439481fda70d08dd3cebac777ccff17f548f74
Iqbal@git22 <iqbalhossainmohon3057@gmail.com>
2024-03-04 11:40:57 +0600

    code update

--- ifElse.c
+++ ifElse.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 main()
 {
-int i, sum ;
+int i, sum = 0;
  //int sum=0;
 for(i=1;i<=10;i=i+1){
     if(i%2==0){
@@ -10,7 +10,6 @@ for(i=1;i<=10;i=i+1){
     else{
             sum=sum-i;
     }
-
 }
     printf("Sum= %d", sum);
 }
