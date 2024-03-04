e62e854ebcf544058e8c5c4cf1eae8d661c20a35
Iqbal@git22 <iqbalhossainmohon3057@gmail.com>
2024-03-05 00:21:51 +0600

    question added

--- ifElseCondition.c
+++ ifElseCondition.c
@@ -1,3 +1,13 @@
+/*
+write a program that will read the value of a variable called "num"
+and print out one of the following messages depending on the value assigned to "num".
+
+    * print "Excellent" if the value of "num" is greater than equal 80.
+    * print "Good" if the value of "num" is greater than equal 50.
+    * Otherwise print "BAD" if the value of "num" is less than 50
+*/
+
+
 #include<stdio.h>
 main(){
     int num;
