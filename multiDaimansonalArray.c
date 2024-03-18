9134b9382455d81f0939690dca1940f8b5e647c2
Iqbal@git22 <iqbalhossainmohon3057@gmail.com>
2024-03-18 19:47:07 +0600

    multi daimansonal array

---
+++ multiDaimansonalArray.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+main() {
+    int x[3][3];
+
+    for(int i = 0; i<3; i++){
+        for(int j = 0; j<3; j++){
+            printf("Please Enter a Numbers: ");
+            scanf("%d", & x[i][j]);
+        }
+    }
+
+    for(int i = 0; i<3; i++){
+        for(int j = 0; j<3; j++){
+            if(i == j){
+                printf(" %d", x[i][j]);
+            }
+        }
+    }
+
+
+    return 0;
+}
