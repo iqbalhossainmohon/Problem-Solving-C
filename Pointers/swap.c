fd05360a38494280da29fd16eba6e7889195e391
Iqbal@git22 <iqbalhossainmohon3057@gmail.com>
2024-04-30 15:06:44 +0600

    swap program added

---
+++ Pointers/swap.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 5, num2 = 10;
+    int *ptr1, *ptr2, temp;
+    //int *ptr1;
+    //int *ptr2;
+     ptr1 = &num1;
+     ptr2 = &num2;
+
+    printf("Before swapping: num1 = %d, num2 = %d\n", num1, num2);
+
+    temp = *ptr1;
+    *ptr1 = *ptr2;
+    *ptr2 = temp;
+
+    printf("After swapping: num1 = %d, num2 = %d\n", num1, num2);
+
+}
